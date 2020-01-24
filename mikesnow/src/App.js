6163d0d77f8e8dd7e464a2f5629f881f517ad629
Aleksandr678 <aleksandr.andrejev678@gmail.com>
2020-01-24 23:48:20 +0200

    next commit

--- mikesnow/src/App.js
+++ mikesnow/src/App.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.';
-import Technologies from './Technologies.';
 
 
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <Technologies />
-        </div>);
+    const App = () => {
+
+      return (
+        <div className='app-wrapper'>
+          <header className='header'>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4s-sbaYkIhfT6qJB8pNBL_N1dKlOWbB1ZaM0Mvpr4ODbHg_DeJw&s'/>
+          </header>
+          <nav className='nav'>
+    <div>
+      <a>Profile</a>
+    </div>
+    <div><a>Messages</a></div>
+    <div><a>Music</a></div>
+    <div><a>Settings</a></div>
+          </nav>
+          
+        
+        <div className='content'>
+          <div>
+          <img src='https://materializecss.com/images/sample-1.jpg' />
+          </div>
+          <div>ava+description
+          
+          </div>
+          <div>My posts</div>
+          <div>New posts</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+        </div>
+      
     
-}
-
-
-export default App;
+    
+    
+    
+    
+    
+             
+     
+          
+      );
+    }
+    
+    export default App;
+    
+    
